4c6dcad1fba836da405c2277d9c154093098bf65
noitorraa <ivankonuk00@gmail.com>
2025-05-08 18:34:45 +0700

    dashbord

---
+++ dashbordForVIRTEX/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace dashbordForVIRTEX.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
